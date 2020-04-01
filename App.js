2f990dea6ae610b101f0db0d52ef308f69403ef8
morelmiles <morelmiles@gmail.com>
2020-04-01 08:21:13 +0300

    Finished all the files

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+import PokemonContainer from "./containers/PokemonContainer";
+
+export function App() {
+  const client = new ApolloClient({
+    uri: "https://graphql.pokemon.now.sh"
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <main>
+        <PokemonContainer />
+      </main>
+    </ApolloProvider>
+  );
+}
---
+++ containers/PokemonContainer.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function PokemonsContainer() {
+  return <p>I am a pokemon container</p>;
+}
